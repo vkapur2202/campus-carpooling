34b6a2396c17b3b12c26b398b6cf3244d74bdece
vkapur2202 <vkk9@case.edu>
2020-10-17 18:52:36 -0500

    unregister mutation added

--- server/src/resolvers/mutation/event.ts
+++ server/src/resolvers/mutation/event.ts
@@ -1,4 +1,4 @@
-import { event as EventType, registration as RegistrationType,user as UserType } from '@prisma/client'
+import { event as EventType, registration as RegistrationType, user as UserType } from '@prisma/client'
 import * as moment from 'moment'
 import { start } from 'repl'
 
@@ -49,21 +49,18 @@ export const Event = {
     return newEvent
   },
 
-  async deleteEvent(parent, { event_date }, ctx: Context): Promise<ISuccessMessage | Error> {
+  async deleteEvent(parent, { event_id }, ctx: Context): Promise<ISuccessMessage | Error> {
     // assumes a user can't have multiple active events at the same time
     if (!ctx.request.userId) {
       throw new AuthError()
     }
     const user = ctx.request.user
 
-    const eventToDelete: EventType = await ctx.prisma.event
-      .findMany({
-        where: {
-          user: user,
-          event_date,
-        },
-      })
-      .then((resp) => resp[0])
+    const eventToDelete: EventType = await ctx.prisma.event.findOne({
+      where: {
+        id: event_id,
+      },
+    })
 
     if (!eventToDelete) {
       throw new Error(`Event could not be found.`)
@@ -145,5 +142,43 @@ export const Event = {
         throw new Error(`Error updating event.`)
       })
     return register
+  },
+
+  async unregister(parent, { event_id }, ctx: Context): Promise<ISuccessMessage | Error> {
+    if (!ctx.request.userId) {
+      throw new AuthError()
+    }
+
+    const user: UserType = ctx.request.user
+
+    const event: EventType = await ctx.prisma.event.findOne({
+      where: {
+        id: event_id,
+      },
+    })
+
+    if (!event) {
+      throw new Error(`Event could not be found`)
+    }
+
+    const registration: RegistrationType = await ctx.prisma.registration
+      .findMany({
+        where: {
+          event_id: event_id,
+          user_id: user.id,
+        },
+      })
+      .then((resp) => resp[0])
+
+    await ctx.prisma.registration
+      .delete({
+        where: {
+          id: registration.id,
+        },
+      })
+      .catch(() => {
+        throw new Error(`Error deleting registration.`)
+      })
+    return { message: 'Registration successfully deleted!' }
   },
 }
