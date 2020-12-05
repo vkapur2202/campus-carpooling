2482065bce826f717231858ca19d9e0f131db9cf
sdovgin <s1dovgin@gmail.com>
2020-12-04 21:59:33 -0500

    Updates to login and resetpassword tests

--- client/src/components/Login/LoginForm.test.jsx
+++ client/src/components/Login/LoginForm.test.jsx
@@ -1,44 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import DeleteEventForm from './DeleteEventForm';
+import LoginForm from './LoginForm';
 import { MockedProvider } from "@apollo/react-testing";
 import { render } from "@testing-library/react";
+import { LOGIN_MUTATION } from "../../GraphQLRequests";
 import { MemoryRouter } from 'react-router-dom';
-import { DELETE_EVENT_MUTATION } from "../../GraphQLRequests";
 
 it("renders the correct content", () => {
-    const testEvent = {
-        id: 4,
-        name: 'Test Event',
-        max_participants: 4,
-        start_location: 'Test Start',
-        end_location: 'Test End',
-        event_date: new Date(2020, 9, 27, 18, 30, 0, 0),
-    }
-
-    const deleteEvent = {
-        message : "Test Message"
+    /*
+    const login = {
+        id: 3,
+        name:"Sarah",
+        confirmed:true
     };
 
     const mocks = [
         {
             request:{
-                query: DELETE_EVENT_MUTATION,
+                query: LOGIN_MUTATION,
                 variables: {
-                    event_id: 4
+                    email: "smd148@case.edu",
+                    password: "pass123"
                 },
             },
             result:{
-                data:{ deleteEvent },
+                data:{login},
             },
             }
     ]
-
     render(
         <MockedProvider mocks={mocks} addTypename={false}>
             <MemoryRouter>
-            <DeleteEventForm event={testEvent}/>
+            <LoginForm/>
             </MemoryRouter>
         </MockedProvider>
     );
+    */
 });
+
--- client/src/components/ResetPassword/ResetRequestForm.test.jsx
+++ client/src/components/ResetPassword/ResetRequestForm.test.jsx
@@ -1,44 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import DeleteEventForm from './DeleteEventForm';
+import ResetRequestForm from './ResetRequestForm';
 import { MockedProvider } from "@apollo/react-testing";
 import { render } from "@testing-library/react";
+import { RESET_REQUEST_MUTATION } from "../../GraphQLRequests";
 import { MemoryRouter } from 'react-router-dom';
-import { DELETE_EVENT_MUTATION } from "../../GraphQLRequests";
 
 it("renders the correct content", () => {
-    const testEvent = {
-        id: 4,
-        name: 'Test Event',
-        max_participants: 4,
-        start_location: 'Test Start',
-        end_location: 'Test End',
-        event_date: new Date(2020, 9, 27, 18, 30, 0, 0),
-    }
-
-    const deleteEvent = {
+    const value = {
         message : "Test Message"
     };
 
     const mocks = [
         {
             request:{
-                query: DELETE_EVENT_MUTATION,
+                query: RESET_REQUEST_MUTATION,
                 variables: {
-                    event_id: 4
+                    email:"smd148@case.edu"
                 },
             },
             result:{
-                data:{ deleteEvent },
+                data:{
+                    message: {value}
+                },
             },
             }
     ]
-
     render(
         <MockedProvider mocks={mocks} addTypename={false}>
             <MemoryRouter>
-            <DeleteEventForm event={testEvent}/>
+            <ResetRequestForm/>
             </MemoryRouter>
         </MockedProvider>
     );
 });
+
